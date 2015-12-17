5dc19f1e9f4dd3044a6c211f91711ec0960db29d
camhux <camzig@gmail.com>
2015-12-16 18:43:22 -0800

    WIP Day 9

---
+++ day9-1.js
@@ -0,0 +1,84 @@
+'use strict';
+let fs = require('fs');
+
+let input = fs.readFileSync(process.argv[2], {encoding: 'utf8'}).split('\n');
+
+let parseLine = function(line) {
+  let temp = line.split(' = ');
+  let cities = temp[0].split(' to ');
+  let distance = parseInt(temp[1], 10);
+
+  return [cities, distance];
+};
+
+let Graph = function() {
+  this.nodes = {};
+};
+
+Graph.prototype.addEdge = function(data) {
+  let fromCity = data[0][0];
+  let toCity = data[0][1];
+  let distance = data[1];
+
+  this.nodes[fromCity] = this.nodes[fromCity] || {edges: {}};
+  this.nodes[fromCity].edges[toCity] = distance;
+};
+
+Graph.prototype.pathFrom = function(fromCity) {
+  let visited = new Set();
+  let dist = 0;
+  let node = this.nodes[fromCity];
+
+  visited.add(fromCity);
+
+  while (visited.size < this.size) {
+    let nearestEdge = getNearestUnvisitedEdge(node.edges, visited);
+    dist += node.edges[nearestEdge];
+    visited.add(nearestEdge);
+    node = this.nodes[nearestEdge];
+  }
+
+  return dist;
+
+};
+
+function getNearestUnvisitedEdge(edges, visited) {
+  let keys = Object.keys(edges).filter(key => !visited.has(key));
+  let nearestEdge = keys[0];
+  let leastDist = edges[keys[0]];
+
+  for (let i = 1; i < keys.length; i++) {
+    let key = keys[i];
+    let dist = edges[key];
+
+    if (dist < leastDist) {
+      leastDist = dist;
+      nearestEdge = key;
+    }
+  }
+
+  return nearestEdge;
+}
+
+Object.defineProperty(Graph.prototype, 'size', {
+  get() {
+    return Object.keys(this.nodes).length;
+  }
+});
+
+// main
+let lines = input.map(parseLine);
+let graph = new Graph();
+
+lines.forEach(graph.addEdge.bind(graph));
+
+let cities = Object.keys(graph.nodes);
+
+let shortestPath = cities.reduce(function(leastDist, city) {
+  let dist = graph.pathFrom(city);
+  if (dist < leastDist) leastDist = dist;
+  return leastDist;
+}, Math.INFINITY);
+
+
+process.stdout.write(shortestPath.toString());
